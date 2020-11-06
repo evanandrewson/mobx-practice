4899e4d4b54095f4ba8a3146eabfab1312f298ce
evanandrewson <evanandrewson@gmail.com>
2020-11-06 11:44:17 -0800

    made provider and hook to use TodoContext

--- src/Provider.jsx
+++ src/Provider.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useLocalObservable } from 'mobx-react';
+import { createTodoStore } from './store';
+import React, { createContext, useContext } from 'react';
+
+const TodoContext = createContext(null);
 
 const TodoProvider = ({ children }) => {
+  const todoStore = useLocalObservable(() => createTodoStore());
 
+  return (
+    <TodoContext.Provider value={todoStore}>
+      {children}
+    </TodoContext.Provider>
+  );
 };
+
+export const useTodoStore = useContext(TodoContext);
 
 export default TodoProvider;
